5d603d9fcd1735dfc694d8a3994f4c9d7221a229
ailymova <aily.mova@gmail.com>
2021-04-28 17:03:17 -0500

    Fixed search method, clean classes, fixed AJAX call (partially)

--- app.js
+++ app.js
@@ -5,9 +5,9 @@
  *
  * ✅ 1. Mostrar una lista en tarjetas de, al menos, los primeros 151 pokemones. Las tarjetas deben mostrar el nombre y
  *    tipo de cada Pokémon (tipo agua, tipo fuego, tipo venenoso, etc.)
- * 2. Permitir que, al hacer click sobre la tarjeta de un pokemon, se despliegue más información, como el peso, sus
+ * 2. TODO: Permitir que, al hacer click sobre la tarjeta de un pokemon, se despliegue más información, como el peso, sus
  *    movimientos (ataques), etc.
- * 3. El sitio web debe tener un buscador de pokemones, donde puedas filtrar pokemones por nombre. (Esto es un plus)
+ * ✅ 3. El sitio web debe tener un buscador de pokemones, donde puedas filtrar pokemones por nombre. (Esto es un plus)
  * Cosas a tener en cuenta:
  * ✅ - Diseño libre (Bootstrap, materialize, o tu propio css)
  * ✅ - Uso de clases e instancias.
@@ -17,6 +17,9 @@
 
 // DOM ELEMENTS
 const pokeContainer = document.getElementById('pokemon__container');
+const searchForm = document.getElementById('search-form');
+const input = document.getElementById('search-input');
+const clearBtn = document.getElementById('clear-btn');
 
 // DATA STRUCTURE
 class Pokemon {
@@ -27,7 +30,7 @@ class Pokemon {
     this.type = [];
     this.getAllInfo();
   }
-  // METHOD: GET RESTO DE LA INFO DEL POKEMÓN
+  // METHOD: GET POKEMON INFO
   getAllInfo() {
     return fetchAndParse(
       this.url,
@@ -37,42 +40,120 @@ class Pokemon {
       this.height = (pokemonData.height / 10).toFixed(2); // in mts
       this.weight = (pokemonData.weight / 10).toFixed(2); // in kgs
       pokemonData.types.forEach(types => this.type.push(types.type.name));
+      // RENDER POKEMON METHOD FROM APP
+      app.renderPokemonCard.call(this, this);
     });
   }
 }
 
 // APP
 class App {
-  constructor() {
+  constructor(noOfPokemons) {
     this.allPokemon = [];
-    this.getPokemonList();
+    // Get all pokemons
+    this.getPokemonList(noOfPokemons);
+    // Event Handlers
+    searchForm.addEventListener('submit', this.searchFor.bind(this));
+    clearBtn.addEventListener('click', this.clearSearchAndRenderAll.bind(this));
   }
 
-  // FUNCT: GET LISTA DE POKEMONES
-  getPokemonList() {
+  // AJAX to get data from API
+  getPokemonList(number) {
     fetchAndParse(
-      'https://pokeapi.co/api/v2/pokemon/?limit=15',
+      `https://pokeapi.co/api/v2/pokemon/?limit=${number}`,
       'Error en la petición'
     )
       .then(data => {
+        // console.log(data.results);
         data.results.forEach((pokemon, index) => {
           this.allPokemon.push(
             new Pokemon(pokemon.name, pokemon.url, index + 1)
           );
         });
         // FIXME: CATCH THIS ERROR
-        return this.allPokemon[this.allPokemon.length - 1].getAllInfo();
+        // return Promise.resolve();
       })
-      .then(() => {
-        this.allPokemon.forEach(pokemon => renderPokemonCard(pokemon));
-      })
+      // .then(() => {
+      //   this.allPokemon.forEach(pokemon => this.renderPokemonCard(pokemon));
+      // })
       .catch(err => console.log(err.message));
   }
 
-  searchFor(name) {}
+  // Render Pokemon Card
+  renderPokemonCard(pokeObj) {
+    let listTypesHtml = '';
+    let [colorA, colorB] = pokeObj.type;
+    // If pokemon has only one type
+    if (!colorB) {
+      colorB = colorA;
+    }
+    // Create type pills
+    pokeObj.type.forEach((type, i, array) => {
+      listTypesHtml += `<p class="py-2 px-4 shadow-md rounded-full text-white font-semibold text-xs mr-2 mb-2 inline-block" style="background: ${typeToColor(
+        type
+      )}">
+    ${type[0].toUpperCase() + type.slice(1)}</p>`;
+    });
+
+    // Build the html string
+    let html = `
+      <div class="card">
+         <img src=${pokeObj.imgSrc} alt="${
+      pokeObj.name
+    }" class="w-full" style="background: linear-gradient(45deg,${typeToColor(
+      colorA
+    )} 0%,${typeToColor(colorA)} 50%,${typeToColor(colorB)} 50%,${typeToColor(
+      colorB
+    )} 100%)">
+         <div class="pokemon-info p-4 text-center">
+             <h2 class="font-light uppercase tracking-wide mb-2">${
+               pokeObj.name
+             }</h2>
+             ${listTypesHtml}
+         </div>
+      </div>`;
+
+    // Add card to pokemon container
+    pokeContainer.insertAdjacentHTML('beforeend', html);
+  }
+
+  // SEARCH
+  searchFor(e) {
+    e.preventDefault();
+    // Get value of input
+    let searchQuery = input.value;
+    // Filter de allPokemon array with searchQuery
+    const found = this.allPokemon.filter(el => el.name.includes(searchQuery));
+
+    // Found array is not empty
+    if (found.length > 0) {
+      // Clear container
+      this.clearContainer();
+      // Show clear btn
+      clearBtn.style.display = 'flex';
+      // Render found pokemons
+      found.forEach(pokemon => this.renderPokemonCard(pokemon));
+    } else {
+      // Render not found
+      console.log('No se encontro ningún pokemon');
+    }
+    // Clear input
+    input.value = '';
+  }
+
+  clearContainer() {
+    pokeContainer.textContent = '';
+  }
+
+  clearSearchAndRenderAll() {
+    this.clearContainer();
+    this.allPokemon.forEach(pokemon => this.renderPokemonCard(pokemon));
+    clearBtn.style.display = 'none';
+  }
 }
 
 // HELPER FUNCT
+// Fetch & parse
 const fetchAndParse = function (url, errMsg) {
   return fetch(url).then(res => {
     if (!res.ok) throw new Error(`${errMsg} (${res.status})`);
@@ -93,41 +174,5 @@ const typeToColor = function (type) {
   return typeColors[type];
 };
 
-// Render Pokemon Card
-const renderPokemonCard = function (pokeObj) {
-  let listTypesHtml = '';
-  let [colorA, colorB] = pokeObj.type;
-
-  if (!colorB) {
-    colorB = colorA;
-  }
-
-  for (let typ of pokeObj.type) {
-    listTypesHtml += `<p class="py-2 px-4 shadow-md rounded-full text-white font-semibold text-xs mr-2 inline-block" style="background: ${typeToColor(
-      typ
-    )}">
-    ${typ}</p>`;
-  }
-
-  let html = `
-  <div class="card">
-     <img src=${pokeObj.imgSrc} alt="${
-    pokeObj.name
-  }" class="w-full" style="background: linear-gradient(45deg,${typeToColor(
-    colorA
-  )} 0%,${typeToColor(colorA)} 50%,${typeToColor(colorB)} 50%,${typeToColor(
-    colorB
-  )} 100%)">
-     <div class="pokemon-info p-4">
-         <h2 class="text-center font-light uppercase tracking-wide">${
-           pokeObj.name
-         }</h2>
-         ${listTypesHtml}
-     </div>
-  </div>
-`;
-  pokeContainer.insertAdjacentHTML('beforeend', html);
-};
-
 // INIT
-const app = new App();
+const app = new App(151);
